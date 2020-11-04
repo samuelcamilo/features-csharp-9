a5f9f0284be72e86b395446cf81a7a5d7c69cb94
Samuel Camilo <samuel.camilo@dellead.com>
2020-11-04 01:49:49 -0300

    included initial project

---
+++ Features.App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Features.App
+{
+    class Program
+    {
+        public static bool StatusCode(int status) => status != 3 ? true : false;
+     
+        public static async Task<bool> ExampleSwitchCase() 
+        {
+            return await Task.FromResult(2).ConfigureAwait(false) switch
+            {
+                (1) => StatusCode(1),
+                (2) => StatusCode(2),
+                _ => StatusCode(3)
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example Switch: " + ExampleSwitchCase().Result); // Example Switch 
+        }
+    }
+}
